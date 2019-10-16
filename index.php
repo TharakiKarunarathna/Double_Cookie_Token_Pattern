3dfb33c9875adb34d47f47c69ce174d938517c2c
TharakiKarunarathna <43366160+TharakiKarunarathna@users.noreply.github.com>
2019-10-16 21:51:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'head.php'; ?>
+<body>
+    <?php include 'common.php'; ?>
+	<div class="container">
+		<div class="row col-sm-4 col-sm-offset-4">
+			<form action="handler.php" method="POST">
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input type="text" class="form-control" id="username" name="username" required value="admin">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" class="form-control" id="password" name="password" required value="admin@123">
+				</div>
+				<button type="submit" class="btn btn-primary">Login</button>
+			</form>
+
+		</div>
+	</div>
+
+</body>
+
+</html>
---
+++ result.php
@@ -0,0 +1,42 @@
+<?php
+    if (isset($_POST['resource_name']) && isset($_POST['csrfToken'])) {
+        $resource = $_POST['resource_name'];
+        $valid_request = false;
+
+        /***
+         * The following code checks if the request csrfToken is 
+         * identical to the csrf token contained in the cookie
+         * If they are equal action would be allowed
+         */
+        if(isset($_COOKIE['tokenID'])){
+            if($_COOKIE['tokenID'] == $_POST['csrfToken']){
+                $valid_request = true;
+            }
+        }
+    }
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'head.php';?>
+<body>
+
+    <?php include 'common.php';?>
+	<div class="container">
+		<div class="row">
+            <?php
+                if ($valid_request) {
+                    echo '<p> Successfully Deleted </p>';
+                } else {
+                    echo '<p> Failed to delete resource </p>';
+                }
+            ?>
+		</div>
+	</div>
+
+</body>
+
+</html>
